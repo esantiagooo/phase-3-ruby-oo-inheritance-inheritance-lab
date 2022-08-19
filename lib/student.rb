96f5ed3d52086ba29f9a6fa1f988f7631963dd74
Eric <eric.yanez50@qmail.cuny.edu>
2022-08-19 10:12:02 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user"
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(knowledge)
+        @knowledge << knowledge
+        # were pushing possible argument or strings into out empty knowledge variable which is empty
+    end 
 end
