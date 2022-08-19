78eeddb6f27cbfefcca66cdac7075ba1bd863347
Eric <eric.yanez50@qmail.cuny.edu>
2022-08-19 10:27:50 -0400

    commit tree 8aba9d49b781a30e8b5f1787c70d5781407dea72

--- lib/student.rb
+++ lib/student.rb
@@ -10,4 +10,7 @@ class Student < User
         @knowledge << knowledge
         # were pushing possible argument or strings into out empty knowledge variable which is empty
     end 
+    def knowledge
+        @knowledge
+    end
 end
